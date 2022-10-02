2bace0be8775865fa7556fe3e3995d1a320fe031
Ayham <ayham.ayham909>
2022-10-02 12:54:05 +0300

    init spring boot hrspring

---
+++ src/test/java/com/global/hrspring/HrSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.global.hrspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
